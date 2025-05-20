b88c6f071813b39502ed123e5150a41a39d8f640
Sai Krishna Vattikunta <krishnavattikuntas@gmail.com>
2025-05-20 11:15:58 -0400

    Inital Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // Reverse a String
+        System.out.println("Enter a String to Reverse");
+        Scanner scanner = new Scanner(System.in);
+        String reverseString = scanner.next();
+       System.out.println("Reversed String is " + reverseString(reverseString));
+    }
+
+    private static String reverseString(String reverseString) {
+        if(reverseString== null || reverseString.length()==1)
+            return reverseString;
+        char[] chars = reverseString.toCharArray();
+        int left =0;
+        int right = chars.length-1;
+        while(left < right){
+            char temp = chars[left];
+            chars[left] = chars [right];
+            chars[right] = temp;
+            right --;
+            left ++;
+        }
+        return new String(chars);
+    }
+}
