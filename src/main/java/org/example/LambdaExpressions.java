11fc69905888c84fe1c9d3f72a04050f0d43081d
Sai Krishna Vattikunta <krishnavattikuntas@gmail.com>
2025-05-25 18:47:08 -0400

    Practice for Lambdas

--- src/main/java/org/example/LambdaExpressions.java
+++ src/main/java/org/example/LambdaExpressions.java
@@ -3,6 +3,8 @@ package org.example;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class LambdaExpressions {
     public static void main(String[] args) {
@@ -30,28 +32,44 @@ public class LambdaExpressions {
         // 1st way
         List<String> stringList = Arrays.asList("Krishna", "Mallika", "Vattikunta", "Linga",
                 "Seetha", "Murali", "Lokesh", "Siva", "Srinu");
-        stringList.sort((name1, name2)-> name1.compareTo(name2));
+        stringList.sort((name1, name2) -> name1.compareTo(name2));
         stringList.forEach(System.out::println);
 
         //2nd way
         stringList.sort(String::compareTo);
         stringList.forEach(System.out::println);
+        //----------------------------------------------------------------------------------
 
         //Question 3)  Create a lambda that adds two integers
-         //traditional way
+        //----------------------------------------------------------------------------------------
+        //traditional way
         int a = 10;
-        int b=20;
-        int c = a+b;
+        int b = 20;
+        int c = a + b;
         System.out.println("----->Sum:" + c);
 
         //Using Lambda..
-      BiFunction<Integer, Integer, Integer> sum = (d, e) -> d + e;
-      System.out.println("Sum using Lamba.." + sum.apply(20,30));
+        BiFunction<Integer, Integer, Integer> sum = (d, e) -> d + e;
+        System.out.println("Sum using Lamba.." + sum.apply(20, 30));
 
+        //---------------------------------------------------------------------------------------
+        //Question 4) Create a lambda that converts a string to uppercase.
+        //----------------------------------------------------------------------------------
+        Function<String, String> s2 = String::toUpperCase;
+        System.out.println(s2.apply("Krishna Loves Mouni"));
 
+        //-----------------------------------------------------------------------------------------------
+
+        //Question 5) Create a lambda that checks if a number is even.
+        //----------------------------------------------------------------------------------------------------------
+
+        Predicate<Integer> checkEven = (n) -> n % 2 == 0;
+        System.out.println("Even Number:" + checkEven.test(9));
+        System.out.println("Even Number:" + checkEven.test(20));
+
+        //-----------------------------------------------------------------------------------------------
 
     }
-
 
 
 }
